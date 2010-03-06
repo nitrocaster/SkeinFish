4cc595d17c08411ec1680bcce8d8c8136ebb125c
novachord <novachord@31c4154c-7a67-11de-98bd-fdc22f85cdda>
2010-03-06 20:34:55 +0000

    update license year

--- trunk/SkeinFish/SkeinFish/Properties/AssemblyInfo.cs
+++ trunk/SkeinFish/SkeinFish/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Alberto Fajardo")]
 [assembly: AssemblyProduct("SkeinFish")]
-[assembly: AssemblyCopyright("Copyright © Alberto Fajardo 2009")]
+[assembly: AssemblyCopyright("Copyright © Alberto Fajardo 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.3.5")]
+[assembly: AssemblyVersion("0.0.4.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
